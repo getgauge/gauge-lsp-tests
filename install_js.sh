88fc54673a32ccad10f5e0d26bde552aa20806d1
Soumya <sswaroop@thoughtworks.com>
2017-11-07 18:55:56 +0530

    using nightly for travis build

---
+++ install_js.sh
@@ -0,0 +1,7 @@
+GAUGE_FILE_NAME="gauge-js-2.0.3.nightly-2017-11-07.zip"
+
+GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/gauge-js/download_file?file_path=$GAUGE_FILE_NAME"
+
+wget $GAUGE_DOWNLOAD_URL
+
+/bin/bash gauge install js -f $GAUGE_FILE_NAME
