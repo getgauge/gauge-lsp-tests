520ee0b80eaac6218a00aabf6c637105b732bff5
Soumya <sswaroop@thoughtworks.com>
2017-11-08 09:42:48 +0530

    adding the file extension

--- install_js.sh
+++ install_js.sh
@@ -2,6 +2,6 @@ GAUGE_FILE_NAME="gauge-js-2.0.3.nightly-2017-11-07.zip"
 
 GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/gauge-js/download_file?file_path=$GAUGE_FILE_NAME"
 
-wget -O jsnightly $GAUGE_DOWNLOAD_URL
+wget -O jsnightly.zip $GAUGE_DOWNLOAD_URL
 
-/bin/bash gauge install js -f ./jsnightly
+/bin/bash gauge install js -f ./jsnightly.zip
