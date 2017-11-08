87bdc1edffac37f602de90baf60286e314bf5662
Soumya <sswaroop@thoughtworks.com>
2017-11-08 11:05:59 +0530

    js installation

--- install_js.sh
+++ install_js.sh
@@ -2,6 +2,6 @@ GAUGE_FILE_NAME="gauge-js-2.0.3.nightly-2017-11-07.zip"
 
 GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/gauge-js/download_file?file_path=$GAUGE_FILE_NAME"
 
-wget -O jsnightly.zip $GAUGE_DOWNLOAD_URL
+wget -O $GAUGE_FILE_NAME $GAUGE_DOWNLOAD_URL
 
-gauge install js -f ./jsnightly.zip
+gauge install js -f ./$GAUGE_FILE_NAME
