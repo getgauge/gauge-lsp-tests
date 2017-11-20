92c1fac63da1c830777982ecccbfba6cfb15b48f
Soumya <sswaroop@thoughtworks.com>
2017-11-20 17:47:14 +0530

    Getting the latest js

--- install_js.sh
+++ install_js.sh
@@ -1,4 +1,6 @@
-GAUGE_FILE_NAME="gauge-js-2.0.3.nightly-2017-11-16.zip"
+$gauge_latest_nightly_version=Invoke-WebRequest -Uri https://bintray.com/gauge/gauge-js/Nightly/_latestVersion -MaximumRedirection 0 -ErrorAction Ignore -UseBasicParsing | %{$_.Headers.Location} | Split-Path -Leaf
+
+GAUGE_FILE_NAME="gauge-js-$($gauge_latest_nightly_version).zip"
 
 GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/gauge-js/download_file?file_path=$GAUGE_FILE_NAME"
 
