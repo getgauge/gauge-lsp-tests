be568cebfbc422d75efc2e659038441e731edd59
Soumya <sswaroop@thoughtworks.com>
2017-11-20 17:43:12 +0530

    Update install_js.sh

--- install_js.sh
+++ install_js.sh
@@ -1,4 +1,4 @@
-GAUGE_FILE_NAME="gauge-js-2.0.3.nightly-2017-11-07.zip"
+GAUGE_FILE_NAME="gauge-js-2.0.3.nightly-2017-11-16.zip"
 
 GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/gauge-js/download_file?file_path=$GAUGE_FILE_NAME"
 
