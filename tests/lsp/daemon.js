f8bece3b4fc2390e3eb0f939c119f4886e21a76d
nehashri <neha.nehe@gmail.com>
2017-12-12 15:54:30 +0530

    starting gauge daemon with correct path

--- tests/lsp/daemon.js
+++ tests/lsp/daemon.js
@@ -2,6 +2,7 @@
 
 const { spawn } = require('child_process');
 const rpc = require('vscode-jsonrpc');
+const vscodeUri = require('vscode-uri').default;
 var path = require('path');
 
 var assert = require('assert');
@@ -11,7 +12,7 @@ var request = require('./request');
 var state = {}
 
 async function startGaugeDaemon(projectPath) {
-    state.projectPath = path.join(cwd, projectPath);
+    state.projectPath = vscodeUri.file(path.join(cwd, projectPath)).fsPath;
     state.gaugeDaemon = spawn('gauge', ['daemon', '--lsp', '--dir=' + state.projectPath, "-l", "debug"],{cwd:state.projectPath});
     state.reader = new rpc.StreamMessageReader(state.gaugeDaemon.stdout);
     state.writer = new rpc.StreamMessageWriter(state.gaugeDaemon.stdin);
