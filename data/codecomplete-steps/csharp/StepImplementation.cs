817c269702b8f61774ee9542f13f73983cc23021
Soumya <sswaroop@thoughtworks.com>
2018-01-15 10:18:45 +0530

    added csharp tests

---
+++ data/codecomplete-steps/csharp/StepImplementation.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Gauge.CSharp.Lib;
+using Gauge.CSharp.Lib.Attribute;
+
+namespace cstest
+{
+    public class StepImplementation
+    {
+        [Step("start")]
+        public void start()
+        {
+        }
+        
+    }
+}
