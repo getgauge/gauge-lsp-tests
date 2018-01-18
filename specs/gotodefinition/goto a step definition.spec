9b70286e628e93788fe00d9cfb525e33aadd23f1
Soumya <sswaroop@thoughtworks.com>
2018-01-18 20:33:51 +0530

    fail fast

--- specs/gotodefinition/goto a step definition.spec
+++ specs/gotodefinition/goto a step definition.spec
@@ -4,18 +4,14 @@ Goto a step definition
 * start gauge daemon for project "data/steps-gotodefinition"
 Goto definition to the implementation of a step from the specification
 ----------------------------------------------------------------------
-* open file "/specs/concept_definition.spec"
-* goto definition of "step1" in "/specs/concept_definition.spec" at "8" and "4" should give details 
-
-   |uri                         |line|range_start|range_end|line_end|
-   |----------------------------|----|-----------|---------|--------|
-   |tests/step_implementation.js|6   |0          |2        |8       |
+* open file "/specs/implementedStep.spec"
+* goto definition of step "step1" in "/specs/implementedStep.spec" at "8" and "4" should give details "implementedStep"
 
 Goto definition of undefined element should give a suitable message
 -------------------------------------------------------------------
 
 * open file "/specs/concept_definition.spec"
-* goto definition of "undefined Concept" in "/specs/concept_definition.spec" at "7" and "4" should give details 
+* goto definition of undefined "undefined Concept" in "/specs/concept_definition.spec" at "7" and "4" should give details 
 
    |error                                                     |
    |----------------------------------------------------------|
--- tests/gotodefinition.js
+++ tests/gotodefinition.js
@@ -3,20 +3,57 @@
 var assert = require('assert');
 var languageclient = require('./lsp/languageclient');
 var builder = require('./lsp/util/dataBuilder');
-step('goto definition of <element> in <relativeFilePath> at <lineNumber> and <characterNumber> should give details <details>',async function(element,relativeFilePath,lineNumber,characterNumber,details,done){
+var YAML = require('yamljs');
+
+step('goto definition of undefined <element> in <relativeFilePath> at <lineNumber> and <characterNumber> should give details <details>',async function(element,relativeFilePath,lineNumber,characterNumber,details){
     try
     {
         var response = await languageclient.gotoDefinition({
             lineNumber:parseInt(lineNumber),characterNumber:parseInt(characterNumber)},
             relativeFilePath);
-    
-        verifyDefinitionResponse(response,details,done)
+        
+        assert.equal(1,2,"Should get the message for the unimplemented step")
     }
     catch(err)
     {
         verifyRejection(err,details)
-        done()
     }
+});
+
+step('goto definition of <element> in <relativeFilePath> at <lineNumber> and <characterNumber> should give details <details>',async function(element,relativeFilePath,lineNumber,characterNumber,details,done){
+    var response;
+    try
+    {
+        response = await languageclient.gotoDefinition({
+            lineNumber:parseInt(lineNumber),characterNumber:parseInt(characterNumber)},
+            relativeFilePath);            
+    }
+    catch(err)
+    {
+        throw new Error('Unable to goto definition '+err)
+    }
+    assert.ok(response!=null,"Response of a defined step should not be null")
+    verifyDefinitionResponse(response,details,done)    
+});
+
+step('goto definition of step <element> in <relativeFilePath> at <lineNumber> and <characterNumber> should give details <data>',async function(element,relativeFilePath,lineNumber,characterNumber,data){
+    var response;
+    var details = YAML.load("specs/gotodefinition/"+data+"/"+process.env.language+"_impl.yaml");      
+
+    try
+    {
+        response = await languageclient.gotoDefinition({
+            lineNumber:parseInt(lineNumber),characterNumber:parseInt(characterNumber)},
+            relativeFilePath);
+    
+        verifyStepDefinitionResponse(response,details)
+    }
+    catch(err)
+    {
+        throw new Error('Unable to goto definition '+err)
+    }
+    assert.ok(response!=null,"Response of a defined step should not be null")
+    verifyStepDefinitionResponse(response,details)    
 });
 
 function verifyRejection(err,details){
@@ -25,6 +62,35 @@ function verifyRejection(err,details){
         assert.equal(err.message,details.rows[0].cells[errorIndex])        
     else
         throw new Error('error not expected '+err)
+}
+
+function verifyStepDefinitionResponse(resp,definitionDetails) {
+    if(resp==null)
+        return null
+    if(resp.message){
+        assert.equal(resp.message,definitionDetails.message)        
+    }
+    else
+    {
+        gauge.message("verify definition")
+        var definitionDetail = definitionDetails[0]
+        
+        var responseMessage = resp        
+        var result = {
+        "range": {
+            "start": {
+            "line": parseInt(definitionDetail.line),
+            "character": parseInt(definitionDetail.range_start)
+            },
+            "end": { "line": parseInt(definitionDetail.line_end), "character": parseInt(definitionDetail.range_end) }
+        },
+        "uri": languageclient.filePath(definitionDetail.uri)
+        };
+
+        var responseUri = builder.getResponseUri(responseMessage.uri)
+        assert.equal(responseUri,result.uri,("response Message uri %s should be equal to %s",responseUri,result.uri))        
+        assert.deepEqual(responseMessage.range, result.range, JSON.stringify(responseMessage.range) + " not equal to " + JSON.stringify(result.range));      
+    }
 }
 
 function verifyDefinitionResponse(resp,definitionDetails,done) {
