5263aac63cc3e098ac36197a4a06ca7ce2126837
BugDiver <vinaysh@thoughtworks.com>
2018-04-04 17:06:41 +0530

    fixing rename tests

--- tests/rename.js
+++ tests/rename.js
@@ -3,24 +3,21 @@ var file = require('./util/fileExtension');
 var builder = require('./lsp/util/dataBuilder')
 var path = require('path')
 var assert = require('assert')
-step("refactor step <details> for project <project>", async function(jsonDetails,project) {
+step("refactor step <details> for project <project>", async function (jsonDetails, project) {
 	var details = builder.loadJSON(jsonDetails)
-	var result = await languageclient.refactor(details.input.uri,details.input.position,details.input.newName)
-	verifyRefactorResult(details.result,result);
+	var result = await languageclient.refactor(details.input.uri, details.input.position, details.input.newName)
+	verifyRefactorResult(details.result, result);
 });
 
-function verifyRefactorResult(expected,actual) {
-	var expected = Object.values(expected.changes);
-	var actual = Object.values(actual.changes);
-
-	for(var i=0;i<expected.length;i++){
-		assert.deepEqual(expected[i], actual[i],"expected "+JSON.stringify(expected[i])+
-		" but was "+JSON.stringify(actual[i]));
+function verifyRefactorResult(expected, actual) {
+	for (k in expected.changes) {
+		var fileUri = file.getUri(languageclient.filePath(k));
+		assert.deepEqual(expected.changes[k], actual.changes[fileUri]);
 	}
 };
 
-step("restore file in project <projectPath> with details <jsonDetails>", async function(projectPath,jsonDetails) {
+step("restore file in project <projectPath> with details <jsonDetails>", async function (projectPath, jsonDetails) {
 	var details = builder.loadJSON(jsonDetails)
-	file.copyFile(file.getFullPath(projectPath,details.input.spec.restoreFrom), file.getFullPath(projectPath,details.input.spec.toBeRestored))	
-	file.copyFile(file.getFullPath(projectPath,details.input.code.restoreFrom), file.getFullPath(projectPath,details.input.code.toBeRestored))	
+	file.copyFile(file.getFullPath(projectPath, details.input.spec.restoreFrom), file.getFullPath(projectPath, details.input.spec.toBeRestored))
+	file.copyFile(file.getFullPath(projectPath, details.input.code.restoreFrom), file.getFullPath(projectPath, details.input.code.toBeRestored))
 });
