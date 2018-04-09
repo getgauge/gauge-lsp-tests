1aa29ed5bb0895562e718948ef289fa022d83ef7
BugDiver <vinaysh@thoughtworks.com>
2018-04-09 18:24:30 +0530

    Fixing file uri.

--- tests/generateCode.js
+++ tests/generateCode.js
@@ -2,23 +2,23 @@ const _fileExtension = require('./util/fileExtension');
 const _customLSP = require('./lsp/customLSP');
 const _assert = require('assert');
 step('delete contents of folder <conceptPath>', async function (conceptPath) {
-    _fileExtension.rmContentsOfDir(_fileExtension.getFullPath(conceptPath));
+	_fileExtension.rmContentsOfDir(_fileExtension.getFullPath(conceptPath));
 });
 step('generate concept <name> in new file under <path> and verify', async function (name, relativePath) {
-    var response = await _customLSP.generateNewConcept(name, relativePath);
-	var conceptFile =  _customLSP.conceptFilePath(relativePath)
+	var response = await _customLSP.generateNewConcept(name, relativePath);
+	var conceptFile = _fileExtension.getUri(_customLSP.conceptFilePath(relativePath))
 	_assert.ok(response.changes[conceptFile]);
-    _assert.equal(response.changes[conceptFile][0].newText, _customLSP.conceptTemplate(name));
+	_assert.equal(response.changes[conceptFile][0].newText, _customLSP.conceptTemplate(name));
 });
 
-step("generate concept <name> in file <fileName> of <filePath> and verify", async function(name,fileName, filePath) {
-	var response = await _customLSP.generateInExistingConcept(name, fileName,filePath);
-	var conceptFile =  _customLSP.conceptFilePath(filePath,fileName)
+step("generate concept <name> in file <fileName> of <filePath> and verify", async function (name, fileName, filePath) {
+	var response = await _customLSP.generateInExistingConcept(name, fileName, filePath);
+	var conceptFile = _fileExtension.getUri(_customLSP.conceptFilePath(filePath, fileName))
 	_assert.ok(response.changes[conceptFile]);
-    _assert.equal(response.changes[conceptFile][0].newText, _customLSP.conceptTemplate(name));
+	_assert.equal(response.changes[conceptFile][0].newText, _customLSP.conceptTemplate(name));
 });
 
-step("get implementation files for project <projectPath>", async function(projectPath) {
+step("get implementation files for project <projectPath>", async function (projectPath) {
 	var files = await _customLSP.getImplFiles();
 	_assert.ok(files.length >= 1);
-  });
+});
