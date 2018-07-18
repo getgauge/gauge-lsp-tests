704429543e4784f58563c865c6e74d45cbd4cb3e
sguptatw <sswaroop@thoughtworks.com>
2018-07-18 18:49:33 +0530

    removing the extra table parameter

--- data/refactor/tests/step_implementation.js
+++ data/refactor/tests/step_implementation.js
@@ -25,10 +25,7 @@ step("Almost all words have vowels <table>", function(table) {
   });
 });
 
-step("something", function(table) {
-  table.rows.forEach(function (row) {
-    assert.equal(numberOfVowels(row.cells[0]), parseInt(row.cells[1]));
-  });
+step("something", function() {
 });
 
 beforeScenario(function () {
