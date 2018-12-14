e10bbdbb7bb088e2cb747ba32bafb4d3f186f2cf
sguptatw <sswaroop@thoughtworks.com>
2018-12-14 21:45:44 +0530

    wrapping & in quotes

--- exec_windows_edit.ps1
+++ exec_windows_edit.ps1
@@ -1,1 +1,1 @@
-path %GAUGE_ROOT%;%PATH% & gauge run --env=$env --tags='!knownIssue & actions_on_file_edit'
+path %GAUGE_ROOT%;%PATH% "& {gauge run --env=$env --tags='!knownIssue & actions_on_file_edit'}"
--- exec_windows_load.ps1
+++ exec_windows_load.ps1
@@ -1,1 +1,1 @@
-path %GAUGE_ROOT%;%PATH% & gauge run --env=$env --tags='!knownIssue & actions_on_project_load'
+path %GAUGE_ROOT%;%PATH% "& {gauge run --env=$env --tags='!knownIssue & actions_on_load_edit'}"
