a4d434ad2098c174cf7713341ad82c303fe8c28d
sguptatw <sswaroop@thoughtworks.com>
2018-12-20 12:57:58 +0530

    fixing the script to run for windows

--- exec_windows_edit.ps1
+++ exec_windows_edit.ps1
@@ -1,1 +1,5 @@
-gauge run --env=$env --tags='!knownIssue & actions_on_file_edit'
+param(
+    [string]$gauge_env = "js"
+)
+$env:PATH="$env:PATH;$env:GAUGE_ROOT" 
+gauge run --env=$gauge_env --tags='!knownIssue & actions_on_file_edit'
