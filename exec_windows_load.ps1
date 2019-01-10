78c24fc2485eab361b3ce5d9cd3dd29c9db1c2dd
Apoorva G A <apoorvag@thoughtworks.com>
2019-01-10 22:08:17 +0530

    Fixing the tag expression

--- exec_windows_load.ps1
+++ exec_windows_load.ps1
@@ -2,4 +2,4 @@ param(
     [string]$gauge_env = "js"
 )
 $env:PATH="$env:PATH;$env:GAUGE_ROOT" 
-gauge run --env=$gauge_env --tags='!knownIssue & actions_on_project_load & actions_on_file_edit'
+gauge run --env=$gauge_env --tags='!knownIssue & (actions_on_project_load | actions_on_file_edit)'
