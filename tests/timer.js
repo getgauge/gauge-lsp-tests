c9c178a6f9040d6ebe3535bef0d6be7893479f88
Nehashri <nehashri@in-Nehazhrl.local>
2019-11-07 15:34:39 +0530

    fixes wait step sleep function

--- tests/timer.js
+++ tests/timer.js
@@ -1,10 +1,3 @@
-step("wait for <secondCount> seconds", function(secondCount) {
-  sleep(secondCount*1000);
+step("wait for <secondCount> seconds", function(secondCount, done) {
+  setTimeout(done, secondCount * 1000);
 });
-
-function sleep(ms)
-{
-  var dt = new Date();
-  dt.setTime(dt.getTime() + ms);
-  while (new Date().getTime() < dt.getTime());
-}
