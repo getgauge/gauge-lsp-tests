bafb0b98a202be4ecea2ca03895304b82b187b39
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-11-08 22:12:58 +0530

    Use real tmp dir path

--- tests/user.js
+++ tests/user.js
@@ -1,40 +1,39 @@
-var tmp = require("tmp");
+var fs = require("fs");
 var ncp = require("ncp").ncp;
 var os = require("os");
 var file = require("./util/fileExtension");
-var tmpobj; 
+var path = require("path");
+var tmpDir;
 
-function copyDataToDir(data,projectDir,done){
-  ncp(data, projectDir,done); 
+function copyDataToDir(data, projectDir, done) {
+  ncp(data, projectDir, done);
 }
 
-function createTempDirectory(){
-  tmpobj = tmp.dirSync();
-  if(os.platform()=="darwin")
-    return "/private"+tmpobj.name;    
-  return tmpobj.name;    
+function createTempDirectory() {
+  tmpDir = fs.mkdtempSync(
+    path.join(fs.realpathSync.native(os.tmpdir()), "gauge-lsp-test")
+  );
+  return tmpDir;
 }
 
-function getProjectDirectory(){
-  return "/private"+tmpobj.name;
+function getProjectDirectory() {
+  return tmpDir;
 }
 
-function removeCallback(){
-  if(tmpobj)
-  {
-    tmpobj.removeCallback();        
+function removeCallback() {
+  if (tmpDir) {
+    tmpDir.removeCallback();
   }
 }
 
-function removeTempDirectory(){
-  if(tmpobj)
-    file.rmDir(tmpobj.name);
+function removeTempDirectory() {
+  if (tmpDir) file.rmDir(tmpDir);
 }
 
-module.exports={
-  createTempDirectory:createTempDirectory,
-  copyDataToDir:copyDataToDir,
-  getProjectDirectory:getProjectDirectory,
-  removeTempDirectory:removeTempDirectory,
-  removeCallBack:removeCallback
+module.exports = {
+  createTempDirectory: createTempDirectory,
+  copyDataToDir: copyDataToDir,
+  getProjectDirectory: getProjectDirectory,
+  removeTempDirectory: removeTempDirectory,
+  removeCallBack: removeCallback
 };
