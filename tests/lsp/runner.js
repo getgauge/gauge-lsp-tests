2f206244967fed8f362f718caf2e3806cf783fd3
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-11-08 22:08:24 +0530

    Add an option to install gauge-ruby gem from a commit or a branch
        Use gauge ruby from a commit hash
        - Set BUILD_FROM_COMMIT_HASH to true
        - Set LATEST_COMMIT_HASH to the commit hash from which you want to build gauge-ruby.
        - Optionally you can set GITHUB_REPOSITORY

--- tests/lsp/runner.js
+++ tests/lsp/runner.js
@@ -14,7 +14,11 @@ function prerequisite(projectPath, runner) {
         var version = JSON.parse(output.toString()).plugins.find(p => p.name == "ruby").version;
         var gemFilePath = path.join(projectPath, "Gemfile");
         var fileContent = file.parseContent(gemFilePath);
-        var newContent = `gem 'gauge-ruby', '~>${version}', :github => 'getgauge/gauge-ruby', :branch => ENV['RUBY_PLUGIN_BRANCH'] || 'master', :group => [:development, :test]`;
+        var newContent = "";
+        if(process.env.BUILD_FROM_COMMIT_HASH == "true" )
+            newContent = `gem 'gauge-ruby', '~>${version}', :github => ENV['GITHUB_REPOSITORY'] || 'getgauge/gauge-ruby', :ref => '${process.env.LATEST_COMMIT_HASH}', :group => [:development, :test]`;
+        else
+            newContent = `gem 'gauge-ruby', '~>${version}', :github => 'getgauge/gauge-ruby', :branch => ENV['RUBY_PLUGIN_BRANCH'] || 'master', :group => [:development, :test]`;
         var result = fileContent.replace(/gem 'gauge-ruby'.*:group => \[:development, :test\]/, newContent);
         file.write(gemFilePath, result);
         var vendorFolderPath = path.join(process.cwd(), "data", "vendor");
