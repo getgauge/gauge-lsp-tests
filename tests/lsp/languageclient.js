fb161abf9ab50a94085ae36118cce7d10d1e46d9
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2022-04-20 13:58:48 +0100

    Fix lint and upgrade packages
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

--- tests/lsp/languageclient.js
+++ tests/lsp/languageclient.js
@@ -1,263 +1,258 @@
 "use strict";
-const vscodeUri = require('vscode-uri').default;
-const file = require('../util/fileExtension');
-const _runner = require('./runner')
-const _lspServer = require('./gauge');
-var path = require('path');
-var fs = require('fs');
-var assert = require('assert');
-const { spawn } = require('child_process');
-const _request = require('./rpc/request');
-const _notification = require('./rpc/notfication');
-const _connection = require('./rpc/connection');
-var builder = require('./util/dataBuilder')
-var cwd = process.cwd();
-var state = {}
+const vscodeUri = require("vscode-uri").default;
+const file = require("../util/fileExtension");
+const _runner = require("./runner");
+const _lspServer = require("./gauge");
+var path = require("path");
+const _request = require("./rpc/request");
+const _notification = require("./rpc/notfication");
+const _connection = require("./rpc/connection");
+var builder = require("./util/dataBuilder");
+var state = {};
 var listeners = [];
 var listenerId = 0;
 
 async function shutDown() {
-    await _request.sendRequest(state.connection, "shutdown", undefined)
-    _notification.sendNotification(state.connection, "exit");
+  await _request.sendRequest(state.connection, "shutdown", undefined);
+  _notification.sendNotification(state.connection, "exit");
 }
 
 function getRange(position){
-    return {
-        "line": parseInt(position.line),
-        "character": parseInt(position.character)        
-    }
+  return {
+    "line": parseInt(position.line),
+    "character": parseInt(position.character)        
+  };
 }
 
 function getMessageParams(fileUri,keyValues) {
-    if(keyValues==null)
-        return null
-    var messageParams = {}
-    for (var index = 0; index < Object.keys(keyValues).length; ++index) {
-        messageParams[Object.keys(keyValues)[index]] = (keyValues[Object.keys(keyValues)[index]]);
-    }
+  if(keyValues==null)
+    return null;
+  var messageParams = {};
+  for (const element of Object.keys(keyValues)) {
+    messageParams[element] = (keyValues[element]);
+  }
 
-    messageParams.textDocument = {
-        "uri": vscodeUri.file(fileUri).toString().replace('%25','%')
-    };
+  messageParams.textDocument = {
+    "uri": vscodeUri.file(fileUri).toString().replace("%25","%")
+  };
 
-    return messageParams;
+  return messageParams;
 }
 
 function codeAction(fileUri,range,diagnostics) {
-    return _request.sendRequest(state.connection,'textDocument/codeAction',getMessageParams(filePath(fileUri),
-        {"range":range,"context":{"diagnostics":diagnostics}}
-    ));
+  return _request.sendRequest(state.connection,"textDocument/codeAction",getMessageParams(filePath(fileUri),
+    {"range":range,"context":{"diagnostics":diagnostics}}
+  ));
 }
 
 function codeLens(fileUri) {
-    return _request.sendRequest(state.connection, 'textDocument/codeLens',getMessageParams(filePath(fileUri),{}))
+  return _request.sendRequest(state.connection, "textDocument/codeLens",getMessageParams(filePath(fileUri),{}));
 }
 
 function codeComplete(position, relativeFilePath) {
-    return _request.sendRequest(state.connection, 'textDocument/completion',getMessageParams(filePath(relativeFilePath),{"position":getRange(position),"context":{"triggerKind":1}}))
+  return _request.sendRequest(state.connection, "textDocument/completion",getMessageParams(filePath(relativeFilePath),{"position":getRange(position),"context":{"triggerKind":1}}));
 }
 
 function gotoDefinition(position, relativeFilePath) {
-    return _request.sendRequest(state.connection, 'textDocument/definition',getMessageParams(filePath(relativeFilePath),{"position":getRange(position)}))
+  return _request.sendRequest(state.connection, "textDocument/definition",getMessageParams(filePath(relativeFilePath),{"position":getRange(position)}));
 }
 
 function workspaceSymbol(params) {
-    return _request.sendRequest(state.connection, 'workspace/symbol', params)
+  return _request.sendRequest(state.connection, "workspace/symbol", params);
 }
 
 function documentSymbol(relativeFilePath) {
-    return _request.sendRequest(state.connection, 'textDocument/documentSymbol',getMessageParams(filePath(relativeFilePath),{}))
+  return _request.sendRequest(state.connection, "textDocument/documentSymbol",getMessageParams(filePath(relativeFilePath),{}));
 }
 
 function formatFile(relativeFilePath) {
-    return _request.sendRequest(state.connection, 'textDocument/formatting',getMessageParams(filePath(relativeFilePath),{"options":{
-        "tabSize": 4,
-        "insertSpaces": true
-    }}))
+  return _request.sendRequest(state.connection, "textDocument/formatting",getMessageParams(filePath(relativeFilePath),{"options":{
+    "tabSize": 4,
+    "insertSpaces": true
+  }}));
 }
 
 function filePath(relativePath) {
-    return path.join(projectPath(), builder.updateSpecsDir(relativePath));
+  return path.join(projectPath(), builder.updateSpecsDir(relativePath));
 }
 
 function projectPath() {
-    if (!state.projectPath)
-        throw ("Project path not set");
-    return state.projectPath;
+  if (!state.projectPath)
+    throw (Error("Project path not set"));
+  return state.projectPath;
 }
 
-function prerequisite(projectPath, runner) {
-    state.projectPath = file.getFSPath(projectPath);
-    _runner.copyManifest(projectPath, runner)
-    _runner.prerequisite(projectPath,runner)
+function prerequisite(gaugeProjectPath, runner) {
+  state.projectPath = file.getFSPath(gaugeProjectPath);
+  _runner.copyManifest(gaugeProjectPath, runner);
+  _runner.prerequisite(gaugeProjectPath,runner);
 }
 
 function refactor(uri, position, newName) {
-    return _request.sendRequest(state.connection, "textDocument/rename", {
-        "textDocument": { "uri": file.getUri(filePath(uri)) },
-        "position": getRange(position),
-        "newName": newName
-    })
+  return _request.sendRequest(state.connection, "textDocument/rename", {
+    "textDocument": { "uri": file.getUri(filePath(uri)) },
+    "position": getRange(position),
+    "newName": newName
+  });
 }
 
 function openProject() {
-    state.gaugeDaemon = _lspServer.startLSP(state.projectPath);
-    return initialize(state.gaugeDaemon, state.projectPath)
-};
+  state.gaugeDaemon = _lspServer.startLSP(state.projectPath);
+  return initialize(state.gaugeDaemon, state.projectPath);
+}
 
 function verificationFailures() {
-    var errorMessage = state.logger.getErrorMessage()
-    return errorMessage
+  return state.logger.getErrorMessage();
 }
 
 function sendRequest(method, params) {
-    return _request.sendRequest(state.connection, method, params)
+  return _request.sendRequest(state.connection, method, params);
 }
 
 
-function saveFile(relativePath, version) {
-    _notification.sendNotification(state.connection, 'textDocument/didSave',
-        {
-            "textDocument":
+function saveFile(relativePath) {
+  _notification.sendNotification(state.connection, "textDocument/didSave",
+    {
+      "textDocument":
                 {
-                    "uri": file.getUri(filePath(relativePath))
+                  "uri": file.getUri(filePath(relativePath))
                 }
-        });
+    });
 }
 
 
 function editFile(relativePath, contentFile) {
-    if (contentFile == null)
-        contentFile = relativePath
+  if (contentFile == null)
+    contentFile = relativePath;
 
-    state.connection.onNotification("textDocument/publishDiagnostics", (res) => { });
+  state.connection.onNotification("textDocument/publishDiagnostics", () => { /* Empty */ });
 
-    _notification.sendNotification(state.connection, 'textDocument/didChange',
-        {
-            "textDocument":
+  _notification.sendNotification(state.connection, "textDocument/didChange",
+    {
+      "textDocument":
                 {
-                    "uri": file.getUri(filePath(relativePath))
+                  "uri": file.getUri(filePath(relativePath))
                 },
-            "contentChanges": [{
-                "text": file.parseContent(filePath(contentFile)),
-            }]
-        });
+      "contentChanges": [{
+        "text": file.parseContent(filePath(contentFile)),
+      }]
+    });
 }
 
 function openFile(relativePath, contentFile) {
-    if (contentFile == null)
-        contentFile = relativePath
+  if (contentFile == null)
+    contentFile = relativePath;
 
-    state.connection.onNotification("textDocument/publishDiagnostics", (res) => { });
-    _notification.sendNotification(state.connection, 'textDocument/didOpen',
-        {
-            "textDocument":
+  state.connection.onNotification("textDocument/publishDiagnostics", () => { /* Empty */});
+  _notification.sendNotification(state.connection, "textDocument/didOpen",
+    {
+      "textDocument":
                 {
-                    "uri": file.getUri(filePath(relativePath)),
-                    "languageId": "markdown",
-                    "version": 1,
-                    "text": file.parseContent(filePath(contentFile))
+                  "uri": file.getUri(filePath(relativePath)),
+                  "languageId": "markdown",
+                  "version": 1,
+                  "text": file.parseContent(filePath(contentFile))
                 }
-        });
+    });
 }
 
 async function initialize(gaugeProcess, execPath) {
-    var result = _connection.newConnection(gaugeProcess)
-    var connection = result.connection
-    state.logger = result.logger
+  var result = _connection.newConnection(gaugeProcess);
+  var connection = result.connection;
+  state.logger = result.logger;
 
-    const initializeParams = getInitializeParams(execPath, gaugeProcess);
+  const initializeParams = getInitializeParams(execPath, gaugeProcess);
 
-    connection.onNotification("window/logMessage", (message) => {
-        console.log(JSON.stringify(message))
+  connection.onNotification("window/logMessage", (message) => {
+    console.log(JSON.stringify(message));
+  });
+
+  connection.onError((e) => {
+    console.log(JSON.stringify(e));
+  });
+
+  await _request.sendRequest(connection, "initialize", initializeParams, null);
+  _notification.sendNotification(connection, "initialized", {});
+
+  var expectedCapabilityIds = ["gauge-fileWatcher", "gauge-runner-didOpen", "gauge-runner-didClose", "gauge-runner-didChange", "gauge-runner-didChange", "gauge-runner-fileWatcher"];
+  var registerCapabilityPromise = new Promise(function (resolve) {
+    _request.onRequest(connection, "client/registerCapability", async (data) => {
+      data.registrations.forEach(registration => {
+        expectedCapabilityIds = expectedCapabilityIds.filter(id => registration.id !== id);
+      });
+      if (expectedCapabilityIds.length == 0) {
+        resolve();
+      }
     });
+  });
 
-    connection.onError((e) => {
-        console.log(JSON.stringify(e));
-    });
+  if (listeners != null && listeners.length > 0)
+    _notification.OnNotification("textDocument/publishDiagnostics", connection, listeners);
 
-    await _request.sendRequest(connection, "initialize", initializeParams, null)
-    _notification.sendNotification(connection, "initialized", {})
-
-    var expectedCapabilityIds = ["gauge-fileWatcher", "gauge-runner-didOpen", "gauge-runner-didClose", "gauge-runner-didChange", "gauge-runner-didChange", "gauge-runner-fileWatcher"];
-    var registerCapabilityPromise = new Promise(async function (resolve, reject) {
-        _request.onRequest(connection, "client/registerCapability", async (data) => {
-            data.registrations.forEach(registration => {
-                expectedCapabilityIds = expectedCapabilityIds.filter(id => registration.id !== id);
-            });
-            if (expectedCapabilityIds.length == 0) {
-                resolve()
-            }
-        })
-    });
-
-    if (listeners != null && listeners.length > 0)
-        _notification.OnNotification("textDocument/publishDiagnostics", connection, listeners)
-
-    state.connection = connection
-    return registerCapabilityPromise
+  state.connection = connection;
+  return registerCapabilityPromise;
 }
 
 // Return the parameters used to initialize a client - you may want to extend capabilities
-function getInitializeParams(projectPath, process) {
-    return {
-        processId: process.pid,
-        rootPath: projectPath,
-        rootUri: vscodeUri.file(projectPath).toString(),
-        capabilities: {
-            workspace: {
-                applyEdit: true,
-                didChangeConfiguration: { dynamicRegistration: true },
-                didChangeWatchedFiles: { dynamicRegistration: true },
-                symbol: { dynamicRegistration: true },
-                executeCommand: { dynamicRegistration: true }
-            },
-            textDocument: {
-                synchronization: { dynamicRegistration: true, willSave: true, willSaveWaitUntil: true, didSave: true },
-                completion: { dynamicRegistration: true, completionItem: { snippetSupport: true, commitCharactersSupport: true } },
-                hover: { dynamicRegistration: true }, signatureHelp: { dynamicRegistration: true },
-                definition: { dynamicRegistration: true },
-                references: { dynamicRegistration: true },
-                documentHighlight: { dynamicRegistration: true }, documentSymbol: { dynamicRegistration: true },
-                codeAction: { dynamicRegistration: true },
-                codeLens: { dynamicRegistration: true },
-                formatting: { dynamicRegistration: true },
-                rangeFormatting: { dynamicRegistration: true },
-                onTypeFormatting: { dynamicRegistration: true },
-                rename: { dynamicRegistration: true },
-                documentLink: { dynamicRegistration: true }
-            }
-        },
-        trace: "off",
-        experimental: {}
-    }
+function getInitializeParams(gaugeProjectPath, process) {
+  return {
+    processId: process.pid,
+    rootPath: gaugeProjectPath,
+    rootUri: vscodeUri.file(gaugeProjectPath).toString(),
+    capabilities: {
+      workspace: {
+        applyEdit: true,
+        didChangeConfiguration: { dynamicRegistration: true },
+        didChangeWatchedFiles: { dynamicRegistration: true },
+        symbol: { dynamicRegistration: true },
+        executeCommand: { dynamicRegistration: true }
+      },
+      textDocument: {
+        synchronization: { dynamicRegistration: true, willSave: true, willSaveWaitUntil: true, didSave: true },
+        completion: { dynamicRegistration: true, completionItem: { snippetSupport: true, commitCharactersSupport: true } },
+        hover: { dynamicRegistration: true }, signatureHelp: { dynamicRegistration: true },
+        definition: { dynamicRegistration: true },
+        references: { dynamicRegistration: true },
+        documentHighlight: { dynamicRegistration: true }, documentSymbol: { dynamicRegistration: true },
+        codeAction: { dynamicRegistration: true },
+        codeLens: { dynamicRegistration: true },
+        formatting: { dynamicRegistration: true },
+        rangeFormatting: { dynamicRegistration: true },
+        onTypeFormatting: { dynamicRegistration: true },
+        rename: { dynamicRegistration: true },
+        documentLink: { dynamicRegistration: true }
+      }
+    },
+    trace: "off",
+    experimental: {}
+  };
 }
 
 function registerForNotification(listener, expectedDiagnostics, verifyIfDone, done) {
-    var id = listenerId
-    listeners.push({ id: listenerId, listener: listener, expectedDiagnostics: expectedDiagnostics, verifyIfDone: verifyIfDone, done: done })
-    listenerId++
-    return id
+  var id = listenerId;
+  listeners.push({ id: listenerId, listener: listener, expectedDiagnostics: expectedDiagnostics, verifyIfDone: verifyIfDone, done: done });
+  listenerId++;
+  return id;
 }
 
 module.exports = {
-    openProject: openProject,
-    registerForNotification: registerForNotification,
-    shutDown: shutDown,
-    openFile: openFile,
-    editFile: editFile,
-    saveFile: saveFile,
-    codeAction:codeAction,
-    codeLens: codeLens,
-    codeComplete:codeComplete,
-    gotoDefinition: gotoDefinition,
-    formatFile: formatFile,
-    filePath: filePath,
-    projectPath: projectPath,
-    verificationFailures: verificationFailures,
-    prerequisite: prerequisite,
-    refactor: refactor,
-    sendRequest: sendRequest,
-    documentSymbol: documentSymbol,
-    workspaceSymbol: workspaceSymbol
-}
+  openProject: openProject,
+  registerForNotification: registerForNotification,
+  shutDown: shutDown,
+  openFile: openFile,
+  editFile: editFile,
+  saveFile: saveFile,
+  codeAction:codeAction,
+  codeLens: codeLens,
+  codeComplete:codeComplete,
+  gotoDefinition: gotoDefinition,
+  formatFile: formatFile,
+  filePath: filePath,
+  projectPath: projectPath,
+  verificationFailures: verificationFailures,
+  prerequisite: prerequisite,
+  refactor: refactor,
+  sendRequest: sendRequest,
+  documentSymbol: documentSymbol,
+  workspaceSymbol: workspaceSymbol
+};
--- tests/lsp/rpc/notfication.js
+++ tests/lsp/rpc/notfication.js
@@ -1,42 +1,42 @@
-const rpc = require('vscode-jsonrpc');
-var assert = require('assert');
+const rpc = require("vscode-jsonrpc");
+var assert = require("assert");
 
 function handleNotifcation(res,registeredHandlers){
-    var results = []
-    for(var i=0;i<registeredHandlers.length;i++){
-        if(registeredHandlers[i].unRegister)
-            continue
-        var expectedDiagnostics = registeredHandlers[i].listener(res,registeredHandlers[i].expectedDiagnostics)
-        var result = registeredHandlers[i].verifyIfDone(expectedDiagnostics);
-        if(result.isValidated)
-        {
-            registeredHandlers[i].done()
-            registeredHandlers[i].unRegister = true    
-        }    
-        results.push(result)
-    }
-    return results;
+  var results = [];
+  for(const element of registeredHandlers){
+    if(element.unRegister)
+      continue;
+    var expectedDiagnostics = element.listener(res,element.expectedDiagnostics);
+    var result = element.verifyIfDone(expectedDiagnostics);
+    if(result.isValidated)
+    {
+      element.done();
+      element.unRegister = true;    
+    }    
+    results.push(result);
+  }
+  return results;
 }
 
-async function OnNotification(notificationType,connection,registeredHandlers,done){
-    await connection.onNotification(notificationType, (res) => {
-        var results = handleNotifcation(res,registeredHandlers)
+async function OnNotification(notificationType,connection,registeredHandlers){
+  await connection.onNotification(notificationType, (res) => {
+    var results = handleNotifcation(res,registeredHandlers);
 
-        if(results!=null && results.length>0 && results[0].errors!=null){
-            var errors = [];
-            for(var index=0;index<results.length;index++){
-                for(var errorIndex=0;errorIndex<results[index].errors.length;errorIndex++){
-                    errors.push(results[index].errors[errorIndex])
-                }                
-            }
-
-            assert.ok(errors.length==0,"diagnostics has errors "+JSON.stringify(errors))
+    if(results!=null && results.length>0 && results[0].errors!=null){
+      var errors = [];
+      for(const element of results){
+        for(const error of element.errors) {
+          errors.push(error);
         }
-    });
+      }
+
+      assert.ok(errors.length==0,"diagnostics has errors "+JSON.stringify(errors));
+    }
+  });
 }
 
 function sendNotification(connection,method,params){
-    connection.sendNotification(new rpc.NotificationType(method), params);
+  connection.sendNotification(new rpc.NotificationType(method), params);
 }
 
-module.exports = {OnNotification:OnNotification,sendNotification:sendNotification}
+module.exports = {OnNotification:OnNotification,sendNotification:sendNotification};
